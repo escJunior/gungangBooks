76f6dd76c3c96aed536e370a2d394a29dc97efaf
escJunior <devescjunior@gmail.com>
2022-09-24 21:21:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# gungangBooks
+ gungangBooks
---
+++ main.js
@@ -0,0 +1,153 @@
+'use strict';
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+
+  scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// Handle click on "Contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  scrollIntoView(link);
+});
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity =
+    1 -
+    (window.scrollY > homeHeight ? homeHeight : window.scrollY) / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUP = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUP.classList.add('visible');
+  } else {
+    arrowUP.classList.remove('visible');
+  }
+});
+
+// Handle Click on the "arrow up" button
+arrowUP.addEventListener('click', (event) => {
+  scrollIntoView('#home');
+});
+
+// 1. 모든 섹션 요소들과 메뉴 아이템들을 가지고 온다.
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다.
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
+
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact',
+];
+
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
+);
+
+let selectedNavIndex = sessionStorage.getItem('selectedNavIndex')
+  ? sessionStorage.getItem('selectedNavIndex')
+  : 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+  selectedNavItem.classList.remove('active');
+  selectedNavItem = selected;
+  selectedNavItem.classList.add('active');
+  sessionStorage.setItem('selectedNavIndex', selectedNavIndex);
+}
+selectNavItem(navItems[selectedNavIndex]);
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+  selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
+
+const observerOptions = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // Scrolling down and Page up
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+sections.forEach((section) => observer.observe(section));
+
+/*window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = sectionIds.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});*/
+
+// To apply navbar select effect when scrolling and mobile device
+window.addEventListener('scroll', () => {
+  if (window.matchMedia('(max-width: 768px)').matches == true) {
+    navbarMenu.classList.remove('open');
+  }
+
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = sectionIds.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
